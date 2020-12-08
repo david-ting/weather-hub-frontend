78304ea5630a80d0db1aeafc59b6c557f0751346
David Ting <davidting@example.com>
2020-12-08 14:09:59 +0800

    change css

--- src/components/chart/LineChoices.js
+++ src/components/chart/LineChoices.js
@@ -4,33 +4,35 @@ function LineChoices({ checkList, changeCheckList }) {
   return (
     <form id="lineChoices">
       Plot: &nbsp;
-      <label className="checkBoxContainer">
-        <span className="checkLabel">temperature</span>
-        <input
-          type="checkbox"
-          checked={checkList.temperature}
-          onChange={() => changeCheckList("temperature")}
-        ></input>
-        <div className="newCheckBox"></div>
-      </label>
-      <label className="checkBoxContainer">
-        <span className="checkLabel">humidity</span>
-        <input
-          type="checkbox"
-          checked={checkList.humidity}
-          onChange={() => changeCheckList("humidity")}
-        ></input>
-        <div className="newCheckBox"></div>
-      </label>
-      <label className="checkBoxContainer">
-        <span className="checkLabel">wind speed</span>
-        <input
-          type="checkbox"
-          checked={checkList.windSpeed}
-          onChange={() => changeCheckList("windSpeed")}
-        ></input>
-        <div className="newCheckBox"></div>
-      </label>
+      <fieldset>
+        <label className="checkBoxContainer">
+          <span className="checkLabel">temperature</span>
+          <input
+            type="checkbox"
+            checked={checkList.temperature}
+            onChange={() => changeCheckList("temperature")}
+          ></input>
+          <div className="newCheckBox"></div>
+        </label>
+        <label className="checkBoxContainer">
+          <span className="checkLabel">humidity</span>
+          <input
+            type="checkbox"
+            checked={checkList.humidity}
+            onChange={() => changeCheckList("humidity")}
+          ></input>
+          <div className="newCheckBox"></div>
+        </label>
+        <label className="checkBoxContainer">
+          <span className="checkLabel">wind speed</span>
+          <input
+            type="checkbox"
+            checked={checkList.windSpeed}
+            onChange={() => changeCheckList("windSpeed")}
+          ></input>
+          <div className="newCheckBox"></div>
+        </label>
+      </fieldset>
     </form>
   );
 }
